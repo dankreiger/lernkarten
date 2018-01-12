668faae5a4f3ae62666a8c659d06023821577cc1
Dan Kreiger <dan@dankreiger.com>
2018-01-12 22:44:56 +0100

    More mobile button styling

--- src/components/Flashcard/FlashcardButtons.js
+++ src/components/Flashcard/FlashcardButtons.js
@@ -6,7 +6,7 @@ const FlashcardButtons = ({previousCard, nextCard, sayWord, mobile}) => {
   return(
     <ButtonToolbar className='flex flex-justify-space-around padding-top-single'>
       {previousCard && <Button bsStyle="primary" bsSize="large" onClick={previousCard}>Previous</Button>}
-      <Button className={classNames({'flex-1-1-100': mobile})} bsSize="large" onClick={sayWord}>Say</Button>
+      <Button className={classNames({'flex-1-1-100': mobile})} bsStyle={mobile ? "primary" : "default"} bsSize="large" onClick={sayWord}>Say</Button>
       {nextCard && <Button bsStyle="primary" bsSize="large" onClick={nextCard}>Next</Button>}
     </ButtonToolbar>
   )
