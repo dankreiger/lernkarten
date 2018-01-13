60664df1c2d23786fd2d5b6460c73d2bb55dc5f6
Dan Kreiger <dan@dankreiger.com>
2018-01-13 16:19:00 +0100

    Add countries to russian vocab

---
+++ src/static/russian/countries.js
@@ -0,0 +1,26 @@
+const countries = [
+  {
+    word: 'Украина',
+    translation: 'Ukraine'
+  }, {
+    word: 'Россия',
+    translation: 'Russia'
+  }, {
+    word: 'Германия',
+    translation: 'Germany'
+  }, {
+    word: 'Америка',
+    translation: 'America'
+  }, {
+    word: 'Китай',
+    translation: 'China'
+  }, {
+    word: 'Belarus',
+    translation: 'Беларусь'
+  }, {
+    word: 'Japan',
+    translation: 'Япония'
+  }
+]
+
+export default countries;
--- src/static/russian/index.js
+++ src/static/russian/index.js
@@ -1,12 +1,14 @@
-import nouns from './nouns';
-import penguin_ch_1 from './penguin_ch_1';
 import greetings from './greetings';
+import nouns from './nouns';
+import countries from './countries';
+import penguin_ch_1 from './penguin_ch_1';
 import bulgakov from './bulgakov';
 import tolstoy from './tolstoy';
 
 const russian = {
   greetings,
   nouns,
+  countries,
   penguin_ch_1,
   bulgakov,
   tolstoy
