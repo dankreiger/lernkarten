ea3e669497e2996f2769a099e9510d708f64e1c6
Dan Kreiger <dan@dankreiger.com>
2018-01-13 04:22:02 +0100

    more vocab

--- src/components/Flashcard/FlashcardButtons.js
+++ src/components/Flashcard/FlashcardButtons.js
@@ -4,7 +4,7 @@ import { ButtonToolbar, Button } from 'react-bootstrap';
 
 const FlashcardButtons = ({previousCard, nextCard, sayWord, mobile}) => {
   return(
-    <ButtonToolbar className='flex flex-justify-space-around padding-top-single'>
+    <ButtonToolbar className='flex flex-justify-space-around full-width button-toolbar'>
       {previousCard && <Button bsStyle="primary" bsSize="large" onClick={previousCard}>Previous</Button>}
       <Button className={classNames({'flex-1-1-100': mobile})} bsStyle={mobile ? "primary" : "default"} bsSize="large" onClick={sayWord}>Say</Button>
       {nextCard && <Button bsStyle="primary" bsSize="large" onClick={nextCard}>Next</Button>}
