9e64d97b787b3833aa5e03c8bcf406d6c7ee21ad
Dan Kreiger <dan@dankreiger.com>
2018-01-13 03:53:26 +0100

    more vocab

--- src/static/russian/bulgakov.js
+++ src/static/russian/bulgakov.js
@@ -5,6 +5,9 @@ const bulgakov = [
   }, {
     word: "Мы говорим с тобой на разных языках, как всегда ... но вещи, о которых мы говорим, от этого не меняются",
     translation: "We speak different languages, as usual ... but this does not change the things we speak about"
+  }, {
+    word: "Надо признаться, что среди интеллигентов тоже попадаются на редкость умные",
+    translation: "I must admit there are some smart people even among the intelligentsia"
   }
 ]
 
