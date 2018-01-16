52d60d9314803278d986481b49f69bafc53751bd
Dan Kreiger <dan@dankreiger.com>
2018-01-17 00:12:48 +0100

    more country vocab

--- src/static/russian/countries.js
+++ src/static/russian/countries.js
@@ -3,29 +3,110 @@ const countries = [
     word: 'Украина',
     translation: 'Ukraine'
   }, {
+    word: 'украинец',
+    translation: 'Ukrainian (m)'
+  }, {
+    word: 'украинка',
+    translation: 'Ukrainian (f)'
+  }, {
+    word: 'украинцы',
+    translation: 'Ukrainians (pl)'
+  }, {
     word: 'Россия',
     translation: 'Russia'
+  }, {
+    word: 'русский',
+    translation: 'Russian (m)'
+  }, {
+    word: 'русская',
+    translation: 'Russian (f)'
+  }, {
+    word: 'русские',
+    translation: 'Russians (pl)'
   }, {
     word: 'Германия',
     translation: 'Germany'
   }, {
+    word: 'немец',
+    translation: 'German (m)'
+  }, {
+    word: 'немка',
+    translation: 'German (f)'
+  }, {
+    word: 'немцы',
+    translation: 'Germans (pl)'
+  }, {
     word: 'Америка',
     translation: 'America'
+  }, {
+    word: 'американец',
+    translation: 'American (m)'
+  }, {
+    word: 'американка',
+    translation: 'American (f)'
+  }, {
+    word: 'американцы',
+    translation: 'Americans (pl)'
   }, {
     word: 'Китай',
     translation: 'China'
   }, {
+    word: 'китайский',
+    translation: 'Chinese (m)'
+  }, {
+    word: 'китайская',
+    translation: 'Chinese (f)'
+  }, {
+    word: 'китайцы',
+    translation: 'Chinese (pl)'
+  }, {
     word: 'Беларусь',
     translation: 'Belarus'
+  }, {
+    word: 'белорусский (m.)',
+    translation: 'Belarus'
+  }, {
+    word: 'белоруска',
+    translation: 'Belarusian (f.)'
+  }, {
+    word: 'белорусы',
+    translation: 'Belarusians (pl.)'
   }, {
     word: 'Япония',
     translation: 'Japan'
   }, {
+    word: 'японский',
+    translation: 'Japanese (m)'
+  }, {
+    word: 'японка',
+    translation: 'Japanese (f)'
+  }, {
+    word: 'японцы',
+    translation: 'Japanese (pl)'
+  }, {
     word: 'Франция',
     translation: 'France'
   }, {
+    word: 'французский',
+    translation: 'French (m)'
+  }, {
+    word: 'француженка',
+    translation: 'French (f)'
+  }, {
+    word: 'французы',
+    translation: 'French (pl)'
+  }, {
     word: 'Испания',
     translation: 'Spain'
+  }, {
+    word: 'испанский',
+    translation: 'Spanish (m)'
+  }, {
+    word: 'испанка',
+    translation: 'Spain (f)'
+  }, {
+    word: 'испанцы',
+    translation: 'Spanish (pl)'
   }
 ]
 
