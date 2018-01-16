8d3cf5fd7fb660e285713d8666b8da36d6f7752a
Dan Kreiger <dan@dankreiger.com>
2018-01-16 02:07:25 +0100

    translate breadcrumbs

--- src/components/BreadcrumbMenu/BreadcrumbMenu.js
+++ src/components/BreadcrumbMenu/BreadcrumbMenu.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Breadcrumb } from 'react-bootstrap';
-import { translateLink, snakeToTitle } from '../../static/helpers';
+import { translateLink, translateTopic } from '../../static/helpers';
 import './BreadcrumbMenu.css';
 
 
 const BreadcrumbMenu = ({history, lesson, currentLocation}) => {
   const path = history.location.pathname,
-        links = translateLink(path);
+        links = translateLink(path),
+        currentLanguage = path.includes('german') ? links.de : links.ru;
 
   return (
     <div className='breadcrumb-wrapper'>
       <Breadcrumb className='container'>
         <Breadcrumb.Item onClick={() => { history.push('/') }}>{links.home}</Breadcrumb.Item>
-        <Breadcrumb.Item onClick={() => { history.push(`/${links.url}`) }}>
-          {path.includes('german') ? links.de : links.ru}
-        </Breadcrumb.Item>
-        {currentLocation && <Breadcrumb.Item active>{snakeToTitle(currentLocation)}</Breadcrumb.Item>}
+        <Breadcrumb.Item onClick={() => { history.push(`/${links.url}`) }}>{currentLanguage}</Breadcrumb.Item>
+        {currentLocation && <Breadcrumb.Item active>{translateTopic(links.url, currentLocation.toLowerCase())}</Breadcrumb.Item>}
       </Breadcrumb>
     </div>
   )
