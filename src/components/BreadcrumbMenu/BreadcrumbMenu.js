c824775f96735cd1d2ce4cbbe0c8722ca43aa887
Dan Kreiger <dan@dankreiger.com>
2018-01-16 03:08:42 +0100

    Try react textfit

--- src/components/BreadcrumbMenu/BreadcrumbMenu.js
+++ src/components/BreadcrumbMenu/BreadcrumbMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Breadcrumb } from 'react-bootstrap';
+import ReactFitText from 'react-fittext';
 import { translateLink, translateTopic } from '../../static/helpers';
 import './BreadcrumbMenu.css';
 
@@ -11,11 +12,13 @@ const BreadcrumbMenu = ({history, lesson, currentLocation}) => {
 
   return (
     <div className='breadcrumb-wrapper'>
-      <Breadcrumb className='container'>
-        <Breadcrumb.Item onClick={() => { history.push('/') }}>{links.home}</Breadcrumb.Item>
-        <Breadcrumb.Item onClick={() => { history.push(`/${links.url}`) }}>{currentLanguage}</Breadcrumb.Item>
-        {currentLocation && <Breadcrumb.Item active>{translateTopic(links.url, currentLocation.toLowerCase())}</Breadcrumb.Item>}
-      </Breadcrumb>
+      <ReactFitText minFontSize={12} compressor={3.5}>
+        <Breadcrumb className='container'>
+          <Breadcrumb.Item onClick={() => { history.push('/') }}>{links.home}</Breadcrumb.Item>
+          <Breadcrumb.Item onClick={() => { history.push(`/${links.url}`) }}>{currentLanguage}</Breadcrumb.Item>
+          {currentLocation && <Breadcrumb.Item active>{translateTopic(links.url, currentLocation.toLowerCase())}</Breadcrumb.Item>}
+        </Breadcrumb>
+      </ReactFitText>
     </div>
   )
 }
--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import classNames from 'classnames';
+import ReactFitText from 'react-fittext';
 
 const Flashcard = ({cardCategory, flipCard, flipped, currentCard}) => {
   const flashcardClasses = ['Flashcard', {'front': !flipped, 'back': flipped}];
 
   return(
     <div className={ classNames('customBgImg', 'notecard', cardCategory, flashcardClasses) } onClick={flipCard}>
-      <p className="lead">{flipped ? currentCard.translation : currentCard.word}</p>
+      <ReactFitText>
+        <p className="lead">{flipped ? currentCard.translation : currentCard.word}</p>
+      </ReactFitText>
     </div>
   );
 }
