cd6657a055c4c0098aba81c5cf44c8a257e3e85d
Dan Kreiger <dan@dankreiger.com>
2018-01-18 02:13:14 +0100

    more

---
+++ src/static/german/schiller.js
@@ -0,0 +1,17 @@
+const schiller = [
+  {
+    word: "Nur der Irrtum ist das Leben, und das Wissen ist der Tod.",
+    translation: "Error alone is life, and knowledge is death."
+  }, {
+    word: "Kannst du nicht allen gefallen durch deine Tat und dein Kunstwerk, mach es wenigen recht; vielen gefallen ist schlimm.",
+    translation: "If you cannot please everyone with your deeds and your art, do it right by a few. Pleasing everyone is bad."
+  }, {
+    word: "Nur die Fülle führt zur Klarheit, und im Abgrund wohnt die Wahrheit.",
+    translation: "Only wholeness leads to clarity, and truth lives in the abyss."
+  }, {
+    word: "Anmut ist die Schönheit der Gestalt unter dem Einfluß der Freiheit.",
+    translation: "Grace is the beauty of form under the influence of freedom."
+  }
+]
+
+export default schiller;
