5ac070a928226c207febdf6eaf81d68a6fb18e57
Dan Kreiger <dan@dankreiger.com>
2018-01-27 18:37:39 +0100

    Remove route to outdated component

--- src/Router.js
+++ src/Router.js
@@ -5,7 +5,6 @@ import vocabulary from './static/vocabulary';
 import Home from './components/Home/Home';
 import LanguageMenu from './components/LanguageMenu/LanguageMenu';
 import FlashcardContainer from './components/Flashcard/FlashcardContainer';
-import QuizMenu from './components/QuizMenu/QuizMenu';
 
 const Router = ({history, location}) => {
   return(
@@ -14,7 +13,6 @@ const Router = ({history, location}) => {
         <Route exact path="/" component={Home}/>
         <Route exact path="/german" component={LanguageMenu}/>
         <Route exact path="/russian" component={LanguageMenu}/>
-        <Route exact path="/quiz" component={QuizMenu}/>
         <Route path="/german/:topic" render={ ()=> <FlashcardContainer history={history} language='german' location={location} words={vocabulary.german}/> } />
         <Route path="/russian/:topic" render={ ()=> <FlashcardContainer history={history} language='russian' location={location} words={vocabulary.russian}/> } />
       </Switch>
