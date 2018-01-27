2102ff920e94dbae8bea33345159e7842f2ae32a
Dan Kreiger <dan@dankreiger.com>
2018-01-27 10:47:36 +0100

    More bug fixing

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -3,20 +3,23 @@ import classNames from 'classnames';
 import Sound from 'react-sound';
 
 const Flashcard = ({cardCategory, nextCard, flipCard, flipped, currentCard, spokenText, language, quizActive}) => {
-  const flashcardClasses = ['Flashcard', {front: !flipped, back: flipped, Quiz: quizActive, incorrect: spokenText !== null && spokenText !== currentCard.word.replace(/\?|!/, ''), correct: spokenText && spokenText === currentCard.word, default: spokenText === null }];
+  const correct = spokenText && spokenText.toLowerCase() == currentCard.word.replace(/\?|!/, '').toLowerCase(),
+        incorrect = spokenText && !correct,
+        flashcardClasses = ['Flashcard', {front: !flipped, back: flipped, Quiz: quizActive, correct, incorrect, default: !spokenText}];
   if(quizActive){
-    if(spokenText && spokenText.toLowerCase() == currentCard.word.replace(/\?|!/, '').toLowerCase()){
-      setTimeout(nextCard, 900);
-    }
+    // if(spokenText && spokenText.toLowerCase() == currentCard.word.replace(/\?|!/, '').toLowerCase()){
+    //   setTimeout(nextCard, 900);
+    // }
     return(
       <div className={ classNames('customBgImg', 'notecard', cardCategory.replace('/quiz', ''), flashcardClasses) } onClick={flipCard}>
         <p className={classNames(`lead ${language}Lead`, {longText: currentCard.word.length > 60})}>{flipped ? currentCard.translation : currentCard.word}</p>
-        {spokenText && spokenText.toLowerCase() == currentCard.word.replace(/\?|!/, '').toLowerCase() &&
+        {correct &&
         <Sound
           url="/audio/correct.mp3"
           playStatus={Sound.status.PLAYING}
+          onFinishedPlaying={() => setTimeout(nextCard, 900)}
         />}
-        {spokenText && spokenText.toLowerCase() != currentCard.word.replace(/\?|!/, '').toLowerCase() &&
+        {incorrect &&
         <Sound
           url="/audio/incorrect.mp3"
           playStatus={Sound.status.PLAYING}
