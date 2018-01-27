e2829e6e0fa48e56b829a324e3542fa47131bab2
Dan Kreiger <dan@dankreiger.com>
2018-01-27 01:13:18 +0100

    make the quiz route work properly

--- src/components/Flashcard/Flashcard.js
+++ src/components/Flashcard/Flashcard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import classNames from 'classnames';
 
-const Flashcard = ({cardCategory, flipCard, flipped, currentCard, language}) => {
-  const flashcardClasses = ['Flashcard', {'front': !flipped, 'back': flipped}];
+const Flashcard = ({cardCategory, flipCard, flipped, currentCard, language, quizActive}) => {
+  const flashcardClasses = ['Flashcard', {'front': !flipped, 'back': flipped, 'Quiz': quizActive}];
 
   return(
-    <div className={ classNames('customBgImg', 'notecard', cardCategory, flashcardClasses) } onClick={flipCard}>
+    <div className={ classNames('customBgImg', 'notecard', cardCategory.replace('/quiz', ''), flashcardClasses) } onClick={flipCard}>
       <p className={classNames(`lead ${language}Lead`, {longText: currentCard.word.length > 60})}>{flipped ? currentCard.translation : currentCard.word}</p>
     </div>
   );
--- src/components/Flashcard/FlashcardContainer.js
+++ src/components/Flashcard/FlashcardContainer.js
@@ -23,25 +23,34 @@ class FlashcardContainer extends Component {
 
     this.cardCategory = formatLink(props.location.pathname);
     this.currentWords = props.words[props.location.pathname.includes('quiz') ? this.cardCategory.split('/')[0].toLowerCase() : this.cardCategory.toLowerCase()];
+    this.currentLocale = props.location.pathname.includes('russian') ? "ru-RU" : "de-DE";
+    this.quizActive = props.location.pathname.includes('quiz');
+
     props.history.listen( location =>  {
-      this.currentLocale = props.location.pathname.includes('russian') ? "ru-RU" : "de-DE";
-      this.setState({quizActive: location.pathname.includes('quiz')})
+      this.quizActive = location.pathname.includes('quiz');
     });
   }
 
   componentWillMount(){
     this.artyom.initialize({
+        debug: true,
         lang: this.currentLocale,
         soundex: true,
     }).then(() => console.log("Artyom has been succesfully initialized"))
       .catch(err => console.error("Artyom couldn't be initialized: ", err));
     this.slowArtyom.initialize({
+        debug: true,
         lang: this.currentLocale,
         soundex: true,
         speed: 0.25,
         mode: "slow"
     }).then(() => console.log("Slow Artyom has been succesfully initialized"))
       .catch(err => console.error("Slow Artyom couldn't be initialized: ", err));
+  }
+
+  componentWillUnmount(){
+    this.artyom = null,
+    this.slowArtyom = null;
   }
 
 
@@ -70,15 +79,14 @@ class FlashcardContainer extends Component {
   }
 
   render() {
-    const {currentCardIndex, flipped, quizActive} = this.state;
+    const {currentCardIndex, flipped} = this.state;
     const currentCard = this.currentWords[currentCardIndex];
     return (
-      <div className="FlashcardContainer">
+      <div className="FlashcardContainer" ref="FlashcardContainer">
         <BreadcrumbMenu history={this.props.history} currentLocation={this.cardCategory} />
         <div className="FlashcardContent">
-          <Flashcard cardCategory={this.cardCategory} flipCard={this.flipCard} flipped={flipped} language={this.props.language} currentCard={currentCard} />
-          {!quizActive && <FlashcardButtons wordQuantity={this.currentWords.length} previousCard={this.previousCard} currentCategory={this.cardCategory} sayWord={this.sayWord} slowSayWord={this.slowSayWord} nextCard={this.nextCard} />}
-          {quizActive && <div>&#9836;</div>}
+          <Flashcard quizActive={this.quizActive} cardCategory={this.cardCategory} flipCard={this.flipCard} flipped={flipped} language={this.props.language} currentCard={currentCard} />
+          {!this.quizActive && <FlashcardButtons wordQuantity={this.currentWords.length} previousCard={this.previousCard} currentCategory={this.cardCategory} sayWord={this.sayWord} slowSayWord={this.slowSayWord} nextCard={this.nextCard} />}
         </div>
       </div>
     )
