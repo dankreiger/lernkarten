cea654447923b2059868bc3f7ada9482d49c59b6
Dan Kreiger <dan@dankreiger.com>
2018-02-17 15:18:49 +0100

    More numbers

--- src/static/russian/numbers.js
+++ src/static/russian/numbers.js
@@ -1,5 +1,4 @@
-const numbers = [
-  {
+const numbers = [{
     word: "один",
     translation: "1"
   }, {
@@ -29,7 +28,96 @@ const numbers = [
   }, {
     word: "десять",
     translation: "10"
-  }
+  },
+  {
+    word: "одиннадцать",
+    translation: "11"
+  }, {
+    word: "двенадцать",
+    translation: "12"
+  }, {
+    word: "тринадцать",
+    translation: "13"
+  }, {
+    word: "четырнадцать",
+    translation: "14"
+  }, {
+    word: "пятнадцать",
+    translation: "15"
+  }, {
+    word: "шестнадцать",
+    translation: "16"
+  }, {
+    word: "семнадцать",
+    translation: "17"
+  }, {
+    word: "восемнадцать",
+    translation: "18"
+  }, {
+    word: "девятнадцать",
+    translation: "19"
+  }, {
+    word: "двадцать",
+    translation: "20"
+  },
+  {
+    word: "тридцать",
+    translation: "30"
+  }, {
+    word: "сорок",
+    translation: "40"
+  }, {
+    word: "пятьдесят",
+    translation: "50"
+  }, {
+    word: "шестьдесят",
+    translation: "60"
+  }, {
+    word: "семьдесят",
+    translation: "70"
+  }, {
+    word: "восемьдесят",
+    translation: "80"
+  }, {
+    word: "девяносто",
+    translation: "90"
+  }, {
+    word: "сто",
+    translation: "100"
+  }, {
+    word: "двести",
+    translation: "200"
+  },{
+    word: "триста",
+    translation: "300"
+  },{
+    word: "четыреста",
+    translation: "400"
+  },{
+    word: "пятьсот",
+    translation: "500"
+  },{
+    word: "шестьсот",
+    translation: "600"
+  },{
+    word: "семьсот",
+    translation: "700"
+  },{
+    word: "восемьсот",
+    translation: "800"
+  },{
+    word: "девятьсот",
+    translation: "900"
+  },{
+    word: "тысяча",
+    translation: "1,000"
+  },{
+    word: "миллион",
+    translation: "1,000,000"
+  },{
+    word: "миллиард",
+    translation: "1,000,000,000"
+  },
 ];
 
 export default numbers;
